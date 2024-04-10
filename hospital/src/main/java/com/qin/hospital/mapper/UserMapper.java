bf70a4c840977e011f7d2b2c4d6d5e792c75f163
WanYue <2650350018@qq.com>
2024-04-10 16:27:55 +0800

    新建用户密码加密

--- hospital/src/main/java/com/qin/hospital/mapper/UserMapper.java
+++ hospital/src/main/java/com/qin/hospital/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.qin.hospital.entity.User;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author WanYue
@@ -16,4 +17,12 @@ public interface UserMapper extends BaseMapper<User> {
      * @return 用户列表
      */
     List<User> getUserList(User user);
+
+    /**
+     * 登录检验
+     * @param map 用户名密码
+     * @return 用户信息
+     */
+    User login(Map<String,String> map);
+
 }
--- hospital/src/main/java/com/qin/hospital/service/UserService.java
+++ hospital/src/main/java/com/qin/hospital/service/UserService.java
@@ -3,6 +3,7 @@ package com.qin.hospital.service;
 import com.qin.hospital.entity.User;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author WanYue
@@ -11,4 +12,6 @@ public interface UserService {
     List<User> getUserList(User user);
 
     int addUser(User user);
+
+    User login(Map<String,String> map);
 }
