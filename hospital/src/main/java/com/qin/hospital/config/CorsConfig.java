6eb529e9237f800fa4a7ee714f66c7a132a4d535
WanYue <2650350018@qq.com>
2024-04-15 15:44:14 +0800

    拦截请求并校验token

--- hospital/src/main/java/com/qin/hospital/config/CorsConfig.java
+++ hospital/src/main/java/com/qin/hospital/config/CorsConfig.java
@@ -1,7 +1,10 @@
 package com.qin.hospital.config;
 
+import com.qin.hospital.filter.WebRequestInterception;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -10,6 +13,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+    /**
+     * 初始化拦截器，防止redisTemplate为null
+     *
+     * @return WebRequestInterception
+     */
+    @Bean
+    public WebRequestInterception getWebRequestInterception() {
+        return new WebRequestInterception();
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 所有接口
@@ -18,5 +31,18 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedMethods(new String[]{"GET", "POST", "PUT", "DELETE"}) // 支持方法
                 .allowedHeaders("*")
                 .exposedHeaders("*");
+    }
+
+    /**
+     * 请求拦截器 拦截除登录注册之外的所有请求
+     *
+     * @param registry 拦截器
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getWebRequestInterception())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/user/login")
+                .excludePathPatterns("/user/register");
     }
 }
---
+++ hospital/src/main/java/com/qin/hospital/filter/WebRequestInterception.java
@@ -0,0 +1,45 @@
+package com.qin.hospital.filter;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author WanYue
+ */
+@Component
+
+public class WebRequestInterception implements HandlerInterceptor {
+
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 获取请求头，判断是否存在token或token是否有效
+     *
+     * @param request  current HTTP request
+     * @param response current HTTP response
+     * @param handler  chosen handler to execute, for type and/or instance evaluation
+     * @return 放行或拦截
+     * @throws Exception 报错
+     */
+    @Override
+    public boolean preHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws Exception {
+        String authorization = request.getHeader("Authorization");
+        if (StringUtils.isEmpty(authorization)) {
+            response.sendError(403, "非法用户");
+            return false;
+        }
+        if (Boolean.FALSE.equals(redisTemplate.hasKey(authorization))) {
+            response.sendError(401, "用户认证过期");
+            return false;
+        }
+        return true;
+    }
+}
